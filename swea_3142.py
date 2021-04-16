9404702044869181a75920e7eaa4992bc0554447
beomsukim <ta09472@naver.com>
2021-04-16 16:18:54 +0900

    조건문 더 좋은 방법 : 수학적 접근

--- swea_3142.py
+++ swea_3142.py
@@ -1,6 +1,24 @@
 t = int(input())
 for i in range(1,t+1):
-    n, m = map(int,input().split())
-    twinhorn = n - m
-    unicon = m - twinhorn
-    print(f"#{i} {unicon} {twinhorn}")
+    n,m,k = map(int,input().split())
+    reached_sec = list(map(int, input().split()))
+    reached_sec.sort()
+    bread = 0
+    idx = 0
+    for c in range(reached_sec[-1]+1):
+        if c != 0: # 0초 제외
+            if c % m == 0:
+                bread += k
+
+        if c == reached_sec[idx]: # 손님이 올 시간이라면
+            if bread == 0: # 남은 붕어빵이 없다면
+                answer = "Impossible"
+                break
+
+            else:
+                bread -= 1
+                idx += 1
+
+        if c == reached_sec[-1]:
+            answer = "Possible"
+    print(f"#{i} {answer}")
