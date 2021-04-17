3f7840d1d6e04cd7ae00f9cf78971715dbd44b38
beomsukim <ta09472@naver.com>
2021-04-17 22:28:33 +0900

    이코테 / greedy / 반복문 보다는 수학적 접근(수열)

---
+++ greedy_02.py
@@ -0,0 +1,11 @@
+n,m,k = map(int, input().split())
+n_list = list(map(int, input().split()))
+n_list.sort(reverse=True)
+
+cnt = m // (k+1) * k
+cnt += m % (k+1)
+
+answer = 0
+answer += (cnt) * n_list[0]
+answer += (m - cnt) * n_list[1]
+print(answer)
