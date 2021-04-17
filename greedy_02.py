6dfaf55417b1913b468d4d2e3bb8870c27738fdc
beomsukim <ta09472@naver.com>
2021-04-17 22:36:52 +0900

    이코테 / 그리디

--- greedy_02.py
+++ greedy_02.py
@@ -1,9 +1,6 @@
 n,m,k = map(int, input().split())
 n_list = list(map(int, input().split()))
-<<<<<<< HEAD
 
-=======
->>>>>>> 3f7840d1d6e04cd7ae00f9cf78971715dbd44b38
 n_list.sort(reverse=True)
 
 cnt = m // (k+1) * k
