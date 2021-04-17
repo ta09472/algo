41b151a6eae695d7e186924f0f80cb4185ad806b
beomsukim <ta09472@naver.com>
2021-04-17 23:24:13 +0900

    swea / 연산

--- swea_3142.py
+++ swea_3142.py
@@ -1,24 +1,6 @@
 t = int(input())
 for i in range(1,t+1):
-    n,m,k = map(int,input().split())
-    reached_sec = list(map(int, input().split()))
-    reached_sec.sort()
-    bread = 0
-    idx = 0
-    for c in range(reached_sec[-1]+1):
-        if c != 0: # 0초 제외
-            if c % m == 0:
-                bread += k
-
-        if c == reached_sec[idx]: # 손님이 올 시간이라면
-            if bread == 0: # 남은 붕어빵이 없다면 
-                answer = "Impossible"
-                break
-
-            else:
-                bread -= 1
-                idx += 1
-
-        if c == reached_sec[-1]:
-            answer = "Possible"
-    print(f"#{i} {answer}")
+    n,m = map(int, input().split())
+    twin_horn = n - m
+    unicon = m - twin_horn
+    print(f"#{i} {unicon} {twin_horn}")
