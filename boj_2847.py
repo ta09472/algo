dc0398103bc99b3519c9dd459d21dde1b2028685
beomsukim <ta09472@naver.com>
2021-04-19 18:18:58 +0900

    boj / 그리디

---
+++ boj_2847.py
@@ -0,0 +1,10 @@
+N = int(input())
+score = []
+cnt = 0
+for _ in range(N):
+    score.append(int(input()))
+for i in range(N - 1, 0, -1):
+    if score[i] <= score[i - 1]:
+        cnt += (score[i - 1] - score[i] + 1)
+        score[i - 1] = score[i] - 1
+print(cnt)
