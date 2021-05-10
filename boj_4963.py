fb96b8a6e2c3d91ca38388bd36cd6d048891569f
beomsukim <ta09472@naver.com>
2021-05-10 14:54:51 +0900

    boj / bfs

---
+++ boj_4963.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = True
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < h and 0 <= ny < w:
+                if graph[nx][ny] == 1 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+    graph = [list(map(int, input().split())) for _ in range(h)]
+    visited = [[False] * w for _ in range(h)]
+    cnt = 0
+
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1 and not visited[i][j]:
+                bfs(i, j)
+                cnt += 1
+    print(cnt)
