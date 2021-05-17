8d27a8865374e215e1b98bddaa5509ce330869d7
beomsukim <ta09472@naver.com>
2021-05-17 14:05:07 +0900

    boj / 정렬

---
+++ boj_11651.py
@@ -0,0 +1,9 @@
+n = int(input())
+x_list = []
+for i in range(n):
+    x, y = map(int, input().split())
+    x_list.append((x,y))
+x_list = sorted(x_list, key = lambda x_list: (x_list[1], x_list[0]))
+
+for k in range(n):
+    print(x_list[k][0], x_list[k][1])
