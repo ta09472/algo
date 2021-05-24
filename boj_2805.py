fb6a92509e265357b3e4ebabbdd62b116693e148
beomsukim <ta09472@naver.com>
2021-05-24 12:12:59 +0900

    boj / 이진 탐색

---
+++ boj_2805.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+woodstock = list(map(int, input().split()))
+
+start = 0
+end = max(woodstock)
+
+result = 0
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+    
+    for wood in woodstock:
+        if wood > mid:
+            total += wood - mid
+
+    if total < m:
+        end = mid - 1
+
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
