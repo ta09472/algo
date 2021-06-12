bae7105dc54b9689810eefaeba96cf830d0f2046
beomsukim <ta09472@naver.com>
2021-06-12 09:52:22 +0900

    boj / 다이나믹 프로그래밍

---
+++ boj_11055.py
@@ -0,0 +1,10 @@
+n = int(input())
+array = list(map(int, input().split()))
+dp = [k for k in array]
+for i in range(n):
+    for j in range(i):
+        if array[i] > array[j]:
+            dp[i] = max(dp[i], dp[j] + array[i])
+
+
+print(max(dp))
