e4d9dedbcdedcbb74095e18d05942a0fd87e0555
beomsukim <ta09472@naver.com>
2021-07-19 09:44:01 +0900

    kakao / 문자열

--- 2019_kakao_winter_intern_02.py
+++ 2019_kakao_winter_intern_02.py
@@ -19,7 +19,18 @@ def solution(s):
 
     return answer
 
+import re
+from collections import Counter
 
+def solution(s):
+    answer = []
 
-s = "{{2},{2,1},{2,1,3},{2,1,3,4}}"
-print(solution(s))
+    s = Counter(re.findall('\d+', s))
+    tmp = sorted(s.items(), key=lambda x: x[1], reverse=True)
+
+    for i in tmp:
+        answer.append(i[0])
+
+    answer = list(map(int, answer))
+
+    return answer
